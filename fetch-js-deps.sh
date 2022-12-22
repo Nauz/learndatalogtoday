d4203bd19c06ee44aa79876e35f7c72a684a00db
Ilya Maximov <mail@overlisted.net>
2022-12-22 11:25:56 +0100

    follow redirects when downloading js deps

--- fetch-js-deps.sh
+++ fetch-js-deps.sh
@@ -5,10 +5,10 @@ set -e
 mkdir -vp resources/public/third-party/pagedown
 mkdir -vp resources/public/third-party/jquery
 
-curl -O http://getbootstrap.com/2.3.2/assets/bootstrap.zip
-curl -O http://codemirror.net/codemirror-3.15.zip
-curl -O https://pagedown.googlecode.com/hg-history/f2a5240e53a726e3c39b92fab9dd25fd9b7614c4/Markdown.Converter.js
-curl -O http://code.jquery.com/jquery-1.10.1.min.js
+curl -LO http://getbootstrap.com/2.3.2/assets/bootstrap.zip
+curl -LO http://codemirror.net/codemirror-3.15.zip
+curl -LO https://pagedown.googlecode.com/hg-history/f2a5240e53a726e3c39b92fab9dd25fd9b7614c4/Markdown.Converter.js
+curl -LO http://code.jquery.com/jquery-1.10.1.min.js
 
 unzip -qu bootstrap.zip -d resources/public/third-party
 unzip -qu codemirror-3.15.zip -d resources/public/third-party
